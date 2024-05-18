321630762a83decd5d537efd1e6b9402d665dd01
Buddhima Fernando <harun.fernando.hf@gmail.com>
2024-05-18 06:50:53 +0530

    hello world

---
+++ tutorial.go
@@ -0,0 +1,11 @@
+// a must to add before starting a new file
+package main
+
+// a standard module allows you to display msgs and texts in the console
+import "fmt"
+
+// whatever goes in this function will be executed once the program runs
+func main() {
+	// this will display the text in the console
+	fmt.Println("Hello world!")
+}
