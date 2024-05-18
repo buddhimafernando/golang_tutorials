a61c9d379f71d7334b6daa9fa70c57fb58f568f5
Buddhima Fernando <harun.fernando.hf@gmail.com>
2024-05-18 07:59:23 +0530

    first 3 exercises

--- tutorial.go
+++ tutorial.go
@@ -4,8 +4,35 @@ package main
 // a standard module allows you to display msgs and texts in the console
 import "fmt"
 
-// whatever goes in this function will be executed once the program runs
 func main() {
-	// this will display the text in the console
+
+	// Exercise 1 - Print hello world
 	fmt.Println("Hello world!")
+
+	// Exercise 2 - Greetings
+	fmt.Println()
+	var name,surname string
+
+	fmt.Print("Please enter your name: ")
+	fmt.Scan(&name, &surname)
+	fmt.Println("My name is "+name+" "+surname)
+
+	// Exercise 3 - Area of a rectangle
+	fmt.Println()
+	var length float32
+	var width float32
+
+	var area float32
+
+	fmt.Print("Please enter the width in cm : ")
+	fmt.Scan(&width)
+	fmt.Print("Please enter the length in cm : ")
+	fmt.Scan(&length)
+
+	area = length * width
+
+	fmt.Println("The area of the rectangle is : ",area,"cm")
+
+	
 }
+
