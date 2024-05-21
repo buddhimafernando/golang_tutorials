bf96486c4118ebce2ee8ea41e38ea38b530167eb
Buddhima Fernando <harun.fernando.hf@gmail.com>
2024-05-21 15:55:17 +0530

    Refactor code to improve readability and remove unused imports

--- tutorial.go
+++ tutorial.go
@@ -4,8 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
-
-	"math/rand"
+	//"math/rand"
 	"strings"
 )
 
@@ -28,7 +27,7 @@ func loan_calculation(amount, rate, time float64){
 }
 
 // Function for exercise 8 - Text analyzer
-func counting_numbers(text string){
+func word_count(text string){
 	var word_count int
 	var words [] string
 
@@ -41,21 +40,24 @@ func counting_numbers(text string){
 
 	// number of characters in the text
 	fmt.Println("characters count = ", len(text))
-	fmt.Println(text[0])
-
-	// find a specific letter in the text
-	//var letter string
-
-	// fmt.Print("Please enter the letter you need to find : ")
-	// fmt.Scan(&letter)
-	
-
-	// for i=0; i<word_count; i++{
-
-	// }
-
-
 }
+
+// find a specific letter in the text
+func find_character(letter,text string){
+	// Trim any newline characters and get the first rune
+	letter = strings.TrimSpace(letter)
+	char := []rune(letter)[0]
+
+	char_count := 0
+	for _, c := range text {
+		if c == char {
+			char_count++
+		}
+	}
+
+	fmt.Printf("Character '%c' count = %d\n", char, char_count)
+}	
+
 
 func main() {
 
@@ -63,122 +65,125 @@ func main() {
 	fmt.Println("Hello world!")
 
 	// Exercise 2 - Greetings
-	fmt.Println()
-	var name,surname string
+	// fmt.Println()
+	// var name,surname string
 
-	fmt.Print("Please enter your name: ")
-	fmt.Scan(&name, &surname)
-	fmt.Println("My name is "+name+" "+surname)
+	// fmt.Print("Please enter your name: ")
+	// fmt.Scan(&name, &surname)
+	// fmt.Println("My name is "+name+" "+surname)
 
-	// Exercise 3 - Area of a rectangle
-	fmt.Println()
-	var length float32
-	var width float32
+	// // Exercise 3 - Area of a rectangle
+	// fmt.Println()
+	// var length float32
+	// var width float32
 
-	var area float32
+	// var area float32
 
-	fmt.Print("Please enter the width in cm : ")
-	fmt.Scan(&width)
-	fmt.Print("Please enter the length in cm : ")
-	fmt.Scan(&length)
+	// fmt.Print("Please enter the width in cm : ")
+	// fmt.Scan(&width)
+	// fmt.Print("Please enter the length in cm : ")
+	// fmt.Scan(&length)
 
-	area = length * width
+	// area = length * width
 
-	fmt.Println("The area of the rectangle is : ",area,"cm")
+	// fmt.Println("The area of the rectangle is : ",area,"cm")
 
-	// Exercise 4 - Simple calculator 
-	fmt.Println()
-	var option, num1, num2, ans float32
+	// // Exercise 4 - Simple calculator 
+	// fmt.Println()
+	// var option, num1, num2, ans float32
 
-	fmt.Println("_Please choose the operation you want to perform_")
-	fmt.Println("addition - 1\nsubstraction - 2\nmultiplication - 3\ndivision - 4")
-	fmt.Print("Please enter the option : ")
-	fmt.Scan(&option)
-	fmt.Print("Please enter the first number : ")
-	fmt.Scan(&num1)
-	fmt.Print("Please enter the second number : ")
-	fmt.Scan(&num2)
+	// fmt.Println("_Please choose the operation you want to perform_")
+	// fmt.Println("addition - 1\nsubstraction - 2\nmultiplication - 3\ndivision - 4")
+	// fmt.Print("Please enter the option : ")
+	// fmt.Scan(&option)
+	// fmt.Print("Please enter the first number : ")
+	// fmt.Scan(&num1)
+	// fmt.Print("Please enter the second number : ")
+	// fmt.Scan(&num2)
 
-	if (option == 1){
-		ans = num1 + num2
-		fmt.Print("Answer = ",ans)
-	}else if(option == 2){
-		ans = num1 - num2
-		fmt.Print("Answer = ",ans)
-	}else if(option == 3){
-		ans = num1 * num2
-		fmt.Print("Answer = ",ans)
-	}else if(option == 4){
-		ans = num1 / num2
-		fmt.Print("Answer = ",ans)
-	}else{
-		fmt.Print("Invalid option")
-	}
+	// if (option == 1){
+	// 	ans = num1 + num2
+	// 	fmt.Print("Answer = ",ans)
+	// }else if(option == 2){
+	// 	ans = num1 - num2
+	// 	fmt.Print("Answer = ",ans)
+	// }else if(option == 3){
+	// 	ans = num1 * num2
+	// 	fmt.Print("Answer = ",ans)
+	// }else if(option == 4){
+	// 	ans = num1 / num2
+	// 	fmt.Print("Answer = ",ans)
+	// }else{
+	// 	fmt.Print("Invalid option")
+	// }
 
-	// Exercise 5 - Number guessing game
-	fmt.Println()
-	var random_num, guess int
-	random_num = rand.Intn(10)
+	// // Exercise 5 - Number guessing game
+	// fmt.Println()
+	// var random_num, guess int
+	// random_num = rand.Intn(10)
 
-	fmt.Print("You got 3 chances to guess the number between 0 and 10\n")
+	// fmt.Print("You got 3 chances to guess the number between 0 and 10\n")
 
-	for i:=0; i<3; i++{
-		fmt.Print("Please enter your guess no ",i+1," : ")
-		fmt.Scan(&guess)
-		if (guess==random_num){
-			fmt.Println("Congratualations! You guessed correctly.")
-			break
-		}else{
-			fmt.Println("Incorrect guess. Let's try again.")
-		}
-	}
-	fmt.Print("\nThe correct number is : ", random_num)
+	// for i:=0; i<3; i++{
+	// 	fmt.Print("Please enter your guess no ",i+1," : ")
+	// 	fmt.Scan(&guess)
+	// 	if (guess==random_num){
+	// 		fmt.Println("Congratualations! You guessed correctly.")
+	// 		break
+	// 	}else{
+	// 		fmt.Println("Incorrect guess. Let's try again.")
+	// 	}
+	// }
+	// fmt.Print("\nThe correct number is : ", random_num)
 
-	// Exercise 6 - Temperature converter
-	fmt.Println()
+	// // Exercise 6 - Temperature converter
+	// fmt.Println()
 
-	var farenheit, celcius, answer float32
-	var choice int
+	// var farenheit, celcius, answer float32
+	// var choice int
 
-	fmt.Println("Please choose the conversion you want to perform\nFarenheit to Celcius - 1\nCelcius to Farenheit - 2")
-	fmt.Print("Please enter the option : ")
-	fmt.Scan(&choice)
-	if (choice == 1){
-		fmt.Print("\nPlease enter the temparature in Fahrenheit : ")
-		fmt.Scan(&farenheit)
-		f_to_c(farenheit,answer)
-	} else if (choice == 2){
-		fmt.Print("\nPlease enter the temparature in Celcius : ")
-		fmt.Scan(&celcius)
-		c_to_f(celcius,	answer)
-	} else{
-		fmt.Print("\nInvalid option")
-	}
+	// fmt.Println("Please choose the conversion you want to perform\nFarenheit to Celcius - 1\nCelcius to Farenheit - 2")
+	// fmt.Print("Please enter the option : ")
+	// fmt.Scan(&choice)
+	// if (choice == 1){
+	// 	fmt.Print("\nPlease enter the temparature in Fahrenheit : ")
+	// 	fmt.Scan(&farenheit)
+	// 	f_to_c(farenheit,answer)
+	// } else if (choice == 2){
+	// 	fmt.Print("\nPlease enter the temparature in Celcius : ")
+	// 	fmt.Scan(&celcius)
+	// 	c_to_f(celcius,	answer)
+	// } else{
+	// 	fmt.Print("\nInvalid option")
+	// }
 
-	// Exercise 7 - Loan calculator
-	fmt.Println()
+	// // Exercise 7 - Loan calculator
+	// fmt.Println()
 
-	var amount, rate, time float64
-	fmt.Print("Please enter the pincipal amount of your loan : ")
-	fmt.Scan(&amount)
-	fmt.Print("Please enter the rate of interest percentage : ")
-	fmt.Scan(&rate)
-	fmt.Print("Please enter the time period(months): ")
-	fmt.Scan(&time)
+	// var amount, rate, time float64
+	// fmt.Print("Please enter the pincipal amount of your loan : ")
+	// fmt.Scan(&amount)
+	// fmt.Print("Please enter the rate of interest percentage : ")
+	// fmt.Scan(&rate)
+	// fmt.Print("Please enter the time period(months): ")
+	// fmt.Scan(&time)
 
-	loan_calculation(amount, rate, time)
+	// loan_calculation(amount, rate, time)
 
 	// Exercise 8 - Text analyzer
 	fmt.Println()
 
-	var text string
+	var text,letter string
 
 	reader := bufio.NewReader(os.Stdin)
 	fmt.Println("Please enter a text : ")
 	text, _ = reader.ReadString('\n')
 	text = strings.TrimSpace(text)
+	word_count(text)
 
-	counting_numbers(text)
+	fmt.Print("Please enter the letter you need to find : ")
+	letter, _ = reader.ReadString('\n')
+	find_character(letter,text)
 
 	// //Exercise 9 - Simple web server
 	// fmt.Println()
